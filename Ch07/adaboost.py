7d6ef6ff3a452d4ccc6af6b364e76cb0aa794e3a
Peter Harrington <peter.b.harrington@gmail.com>
2012-01-21 10:11:51 -0800

    changed labels in last function

--- Ch07/adaboost.py
+++ Ch07/adaboost.py
@@ -5,13 +5,26 @@ Adaboost is short for Adaptive Boosting
 '''
 from numpy import *
 
-def loadDataSet(fileName):
+def loadSimpData():
+    datMat = matrix([[ 1. ,  2.1],
+        [ 2. ,  1.1],
+        [ 1.3,  1. ],
+        [ 1. ,  1. ],
+        [ 2. ,  1. ]])
+    classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
+    return datMat,classLabels
+
+def loadDataSet(fileName):      #general function to parse tab -delimited floats
+    numFeat = len(open(fileName).readline().split('\t')) #get number of fields 
     dataMat = []; labelMat = []
     fr = open(fileName)
     for line in fr.readlines():
-        lineArr = line.strip().split('\t')
-        dataMat.append([float(lineArr[0]), float(lineArr[1])])
-        labelMat.append(float(lineArr[2]))
+        lineArr =[]
+        curLine = line.strip().split('\t')
+        for i in range(numFeat-1):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
     return dataMat,labelMat
 
 def stumpClassify(dataMatrix,dimen,threshVal,threshIneq):#just classify the data
@@ -37,9 +50,8 @@ def buildStump(dataArr,classLabels,D):
                 predictedVals = stumpClassify(dataMatrix,i,threshVal,inequal)#call stump classify with i, j, lessThan
                 errArr = mat(ones((m,1)))
                 errArr[predictedVals == labelMat] = 0
-                print "predictedVals",predictedVals.T,"errArr",errArr.T
                 weightedError = D.T*errArr  #calc total error multiplied by D
-                print "split: dim %d, thresh %.2f, thresh ineqal: %s, the weighted error is %.3f" % (i, threshVal, inequal, weightedError)
+                #print "split: dim %d, thresh %.2f, thresh ineqal: %s, the weighted error is %.3f" % (i, threshVal, inequal, weightedError)
                 if weightedError < minError:
                     minError = weightedError
                     bestClasEst = predictedVals.copy()
@@ -48,29 +60,66 @@ def buildStump(dataArr,classLabels,D):
                     bestStump['ineq'] = inequal
     return bestStump,minError,bestClasEst
 
-def adaBoostTrain(dataArr,classLabels,numIt=40):
+
+def adaBoostTrainDS(dataArr,classLabels,numIt=40):
     weakClassArr = []
     m = shape(dataArr)[0]
     D = mat(ones((m,1))/m)   #init D to all equal
     aggClassEst = mat(zeros((m,1)))
     for i in range(numIt):
         bestStump,error,classEst = buildStump(dataArr,classLabels,D)#build Stump
-        #print "error",error
+        #print "D:",D.T
         alpha = float(0.5*log((1.0-error)/max(error,1e-16)))#calc alpha, throw in max(error,eps) to account for error=0
         bestStump['alpha'] = alpha  
-        print "alpha",alpha
-        weakClassArr.append(bestStump)#store Stump Params in Array
-        print "classEst",classEst
+        weakClassArr.append(bestStump)                  #store Stump Params in Array
+        #print "classEst: ",classEst.T
         expon = multiply(-1*alpha*mat(classLabels).T,classEst) #exponent for D calc, getting messy
-        D = multiply(D,exp(expon)) #Calc New D, element-wise 
+        D = multiply(D,exp(expon))                              #Calc New D for next iteration
         D = D/D.sum()
-        print "D",D
         #calc training error of all classifiers, if this is 0 quit for loop early (use break)
         aggClassEst += alpha*classEst
-        print "aggClassEst",aggClassEst
+        #print "aggClassEst: ",aggClassEst.T
         aggErrors = multiply(sign(aggClassEst) != mat(classLabels).T,ones((m,1)))
-        #print aggErrors
         errorRate = aggErrors.sum()/m
-        print errorRate
+        print "total error: ",errorRate
         if errorRate == 0.0: break
-    return weakClassArr
+    return weakClassArr,aggClassEst
+
+def adaClassify(datToClass,classifierArr):
+    dataMatrix = mat(datToClass)#do stuff similar to last aggClassEst in adaBoostTrainDS
+    m = shape(dataMatrix)[0]
+    aggClassEst = mat(zeros((m,1)))
+    for i in range(len(classifierArr)):
+        classEst = stumpClassify(dataMatrix,classifierArr[i]['dim'],\
+                                 classifierArr[i]['thresh'],\
+                                 classifierArr[i]['ineq'])#call stump classify
+        aggClassEst += classifierArr[i]['alpha']*classEst
+        print aggClassEst
+    return sign(aggClassEst)
+
+def plotROC(predStrengths, classLabels):
+    import matplotlib.pyplot as plt
+    cur = (1.0,1.0) #cursor
+    ySum = 0.0 #variable to calculate AUC
+    numPosClas = sum(array(classLabels)==1.0)
+    yStep = 1/float(numPosClas); xStep = 1/float(len(classLabels)-numPosClas)
+    sortedIndicies = predStrengths.argsort()#get sorted index, it's reverse
+    fig = plt.figure()
+    fig.clf()
+    ax = plt.subplot(111)
+    #loop through all the values, drawing a line segment at each point
+    for index in sortedIndicies.tolist()[0]:
+        if classLabels[index] == 1.0:
+            delX = 0; delY = yStep;
+        else:
+            delX = xStep; delY = 0;
+            ySum += cur[1]
+        #draw line from cur to (cur[0]-delX,cur[1]-delY)
+        ax.plot([cur[0],cur[0]-delX],[cur[1],cur[1]-delY], c='b')
+        cur = (cur[0]-delX,cur[1]-delY)
+    ax.plot([0,1],[0,1],'b--')
+    plt.xlabel('False positive rate'); plt.ylabel('True positive rate')
+    plt.title('ROC curve for AdaBoost horse colic detection system')
+    ax.axis([0,1,0,1])
+    plt.show()
+    print "the Area Under the Curve is: ",ySum*xStep
