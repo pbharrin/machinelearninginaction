adf2a08ad95abcdc5c498e91b6b1f4a4c58e80bd
Jim Haughwout <jim.haughwout@gmail.com>
2014-01-11 13:01:25 -0500

    Update kNN.py
    Resolves 'invalid literal for int() with base 10' error (input file in example has text labels)

--- Ch02/kNN.py
+++ Ch02/kNN.py
@@ -45,7 +45,7 @@ def file2matrix(filename):
         line = line.strip()
         listFromLine = line.split('\t')
         returnMat[index,:] = listFromLine[0:3]
-        classLabelVector.append(int(listFromLine[-1]))
+        classLabelVector.append(listFromLine[-1])
         index += 1
     return returnMat,classLabelVector
     
