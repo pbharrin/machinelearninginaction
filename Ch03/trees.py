e2a4b3d686c023f0e93eb82a39bd8b916a004f01
nicozhang <315393472@qq.com>
2018-02-18 10:16:26 +0800

    modify code for more pythonic
    I am not suer writing like that maybe more pythonic? : )

--- Ch03/trees.py
+++ Ch03/trees.py
@@ -21,8 +21,9 @@ def calcShannonEnt(dataSet):
     labelCounts = {}
     for featVec in dataSet: #the the number of unique elements and their occurance
         currentLabel = featVec[-1]
-        if currentLabel not in labelCounts.keys(): labelCounts[currentLabel] = 0
-        labelCounts[currentLabel] += 1
+        labelCounts[currentLabel] = labelCounts.get('currentLabel', 0) + 1 
+        #if currentLabel not in labelCounts.keys(): labelCounts[currentLabel] = 0
+        #labelCounts[currentLabel] += 1
     shannonEnt = 0.0
     for key in labelCounts:
         prob = float(labelCounts[key])/numEntries
